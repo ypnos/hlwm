75209b55e53f571e0891dd29f31cf424d101bc48
ypnos <git@lanrules.de>
2014-09-03 02:23:59 +0200

    fix timetrack to have a grace period before reporting. so not mix-up titles and tags due to the title change being announced first, also not reporting on quick cycling through windows. first version of a script that parses the logfile

--- timetrack.pl
+++ timetrack.pl
@@ -1,52 +1,69 @@
 #!/usr/bin/perl
 # This script tracks usage of windows / tags / activities over time.
-
+use v5.20;
+use IO::Select;
+use Time::HiRes qw(time); # time() delivers floating seconds
 use POSIX qw(strftime);
+use sigtrap qw/handler lastsupper normal-signals/;
 
-my ($logfile) = @ARGV;
-die "Usage: timetrack.pl <name of logfile>\n" unless $logfile;
-
-my $activity = "undefined";
-my $tag = "undefined";
-my $title = "undefined";
+my $activity = 0;
+my $tag = 0;
+my $title = 0;
+my $lastevent = -1;
 
 sub timestamp
 {
-	my $msg = $_[0];
-	my $ts = time;
-	my $dt = strftime "%Y-%m-%d%t%T", localtime;
-	print("$dt\t$ts\t$msg\n");
+	my ($ts, $msg) = @_;
+	my $dt = strftime "%Y-%m-%d%t%T", localtime($ts);
+	print("$dt\t$ts\t$msg\n") if $title;
+}
+
+sub commit
+{
+	if ($lastevent > (time - 0.25)) {
+		#print "holding off -> ".time." vs $lastevent \n";
+	} else {
+		timestamp($lastevent, "$activity\t$tag\t$title");
+	}
+	$lastevent = time;
+}
+
+sub lastsupper
+{
+	commit() and exit;
 }
 
 sub update_activity
 {
+	commit();
 	$activity = $_[1];
 }
 
 sub update_tag
 {
+	commit();
 	$tag = $_[1];
 }
 
 sub update_title
 {
+	commit();
 	$title = $_[2];
-	timestamp("$activity\t$tag\t$title");
 }
 
 sub enter_void
 {
+	$lastevent = -1;
 	timestamp("inactive");
 }
 
 sub leave_void
 {
+	$lastevent = -1;
 	timestamp("$activity\t$tag\t$title");
 }
 
 ## main routine
-use v5.20;
-use IO::Select;
 
 # set up pipes
 open my $hhandle, "herbstclient -i '(activity_changed|tag_changed|focus_changed|reload)'|"
@@ -60,7 +77,7 @@ while(my @ready = $sel->can_read) {
 	foreach my $fh (@ready) {
 		my $buffer = readline($fh);
 		chomp $buffer;
-		print "************************ $buffer\n";
+		#print "************************ $buffer\n";
 		for ($buffer) {
 			update_activity(split(/\t/)) when /^activity_changed/;
 			update_tag(split(/\t/)) when /^tag_changed/;
@@ -72,6 +89,3 @@ while(my @ready = $sel->can_read) {
 		}
 	}
 }
-
-close($hhandle) or die "unfinished love story: $! $?\n"; # happens on hlwm crash
-close($shandle) or die "unfinished hate story: $! $?\n"; # happens?
